d442dac9197997327c00a8bd6cf7b6add988570a
Snap <XxgaugerxX@gmail.com>
2024-08-26 21:00:44 -0700

    Add create() method to static OutputDataFile class.

--- app/src/main/java/beppy/OutputDataFile.java
+++ app/src/main/java/beppy/OutputDataFile.java
@@ -2,6 +2,7 @@ package beppy;
 
 import java.nio.file.Path;
 import java.nio.file.Files;
+import java.io.IOException;
 
 final class OutputDataFile
 {
@@ -12,4 +13,17 @@ final class OutputDataFile
     final static Path PATH = Path.of(RootDir.PATH.toString(), OutputDataFile.NAME);
 
     static boolean exists() { return Files.exists(PATH); }
+
+    static void create()
+    {
+        try
+        {
+            Files.createDirectory(OutputDataFile.PATH);
+        }
+        catch(IOException err)
+        {
+            err.printStackTrace();
+            System.exit(222);
+        }
+    }
 }
