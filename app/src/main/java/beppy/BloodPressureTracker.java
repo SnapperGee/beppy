02e7ea19a6260b873e19dc46fe19802b3375cbf0
Snap <XxgaugerxX@gmail.com>
2024-08-27 00:31:52 -0700

    Remove unused JFrame classes.

--- app/src/main/java/beppy/BloodPressureTracker.java
+++ app/src/main/java/beppy/BloodPressureTracker.java
@@ -1,10 +1,7 @@
 package beppy;
 
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import java.lang.reflect.InvocationTargetException;
 
 class RootWindow implements Runnable
 {
@@ -19,7 +16,8 @@ class RootWindow implements Runnable
 
 public class BloodPressureTracker {
 
-	public static void main(String args[]) throws InvalidOSException, IOException {
+	public static void main(String args[])
+	{
 		if( ! OutputDataFile.exists())
 		{
 			System.out.println("Output data file not found.Creating output data file...");
@@ -29,54 +27,5 @@ public class BloodPressureTracker {
 		System.out.printf("Using output data file: \"%s\"", OutputDataFile.PATH);
 
 		SwingUtilities.invokeLater(new RootWindow());
-
-		// try
-		// {
-		// 	SwingUtilities.invokeAndWait(
-		// 		new Runnable() {
-		// 			public void run()
-		// 			{
-		// 				new RootWindow();
-		// 			}
-		// 		}
-		// 	);
-		// }
-		// catch (InvocationTargetException err)
-		// {
-		// 	err.printStackTrace();
-		// 	System.exit(8);
-		// }
-		// catch (InterruptedException err)
-		// {
-		// 	err.printStackTrace();
-		// 	System.exit(9);
-		// }
-
-
-
-
-
-
-
-
-		// FileIO config = new FileIO();
-		// if (config.doesExist(0)) {
-		// 	System.out.println("Config file found. Loading config...");
-		// } else {
-		// 	CountDownLatch latch = new CountDownLatch(1);
-		// 	new SetupWindow(config, latch);
-		// 	try {
-		// 		// Wait until the SetupWindow completes (i.e., latch.countDown() is called)
-		// 		latch.await();
-		// 	} catch (InterruptedException e) {
-		// 		e.printStackTrace();
-		// 	}
-		// }
-		// if (config.doesExist(1)) {
-		// 	System.out.println("Entry log found at: " + config.readProperties());
-		// } else {
-		// 	config.writeEntries();
-		// }
-		// new AddEntryWindow();
 	}
 }
