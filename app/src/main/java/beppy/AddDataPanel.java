c22874aacb3fe22019d09ec1e68b4d26a7c79da4
Snap <XxgaugerxX@gmail.com>
2024-08-27 13:03:31 -0700

    Init AddDataPanel class.

---
+++ app/src/main/java/beppy/AddDataPanel.java
@@ -0,0 +1,26 @@
+package beppy;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+final class AddDataPanel
+{
+    final static JPanel create()
+    {
+        final JPanel buttonAndInputFieldsPane = new JPanel();
+
+        final JButton addButton = new JButton("Add");
+
+        addButton.setName("Add Button");
+
+        final JTextField systolicTextField = new JTextField(10);
+        final JTextField diastolicTextField = new JTextField(10);
+
+        buttonAndInputFieldsPane.add(addButton);
+        buttonAndInputFieldsPane.add(systolicTextField);
+        buttonAndInputFieldsPane.add(diastolicTextField);
+
+        return buttonAndInputFieldsPane;
+    }
+}
