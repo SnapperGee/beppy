022a59bf0c077443db9d0217ccf93ead5e5a0114
Snap <XxgaugerxX@gmail.com>
2024-08-27 00:44:35 -0700

    Remove apache poi package dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,6 @@ repositories {
 dependencies {
     testImplementation(libs.junit.jupiter)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.apache.poi:poi:5.3.0")
-    implementation("org.apache.poi:poi-ooxml:5.3.0")
 }
 
 java {
