37e5a18c0662c6d44eedf93b01f749890d952c64
Snap <XxgaugerxX@gmail.com>
2024-08-27 02:30:01 -0700

    Use spaces for indentation.

--- app/src/main/java/beppy/Beppy.java
+++ app/src/main/java/beppy/Beppy.java
@@ -9,40 +9,40 @@ import java.awt.Color;
 
 final class RootWindow implements Runnable
 {
-	public void run()
-	{
-		UIDefaults defaults = UIManager.getLookAndFeelDefaults();
-		defaults.putIfAbsent("Table.alternateRowColor", Color.LIGHT_GRAY);
+    public void run()
+    {
+        UIDefaults defaults = UIManager.getLookAndFeelDefaults();
+        defaults.putIfAbsent("Table.alternateRowColor", Color.LIGHT_GRAY);
 
-		final JFrame rootJFrame = new JFrame();
-		rootJFrame.setName("Beppy");
-		rootJFrame.setTitle("Beppy");
-		rootJFrame.setSize(500, 300);
+        final JFrame rootJFrame = new JFrame();
+        rootJFrame.setName("Beppy");
+        rootJFrame.setTitle("Beppy");
+        rootJFrame.setSize(500, 300);
 
-		final String[][] data = Arrays.copyOfRange(DataFile.readTo2dArray(), 1, DataFile.readTo2dArray().length);
+        final String[][] data = Arrays.copyOfRange(DataFile.readTo2dArray(), 1, DataFile.readTo2dArray().length);
 
-		final var table = ScrollableDataTableFrame.create(data);
+        final var table = ScrollableDataTableFrame.create(data);
 
-		rootJFrame.add(table);
+        rootJFrame.add(table);
 
-		rootJFrame.setVisible(true);
-	}
+        rootJFrame.setVisible(true);
+    }
 }
 
 public class Beppy
 {
 
-	public static void main(String args[])
-	{
-		if( ! DataFile.exists())
-		{
-			System.out.println("Data file not found. Creating output data file...");
-			final var path = DataFile.create();
-			System.out.printf("Data file created at: \"%s\"", path);
-		}
+    public static void main(String args[])
+    {
+        if( ! DataFile.exists())
+        {
+            System.out.println("Data file not found. Creating output data file...");
+            final var path = DataFile.create();
+            System.out.printf("Data file created at: \"%s\"", path);
+        }
 
-		System.out.printf("Using output data file: \"%s\"", DataFile.PATH);
+        System.out.printf("Using output data file: \"%s\"", DataFile.PATH);
 
-		SwingUtilities.invokeLater(new RootWindow());
-	}
+        SwingUtilities.invokeLater(new RootWindow());
+    }
 }
